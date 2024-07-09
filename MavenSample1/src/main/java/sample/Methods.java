89b0cfca49625e721bca1b485179bc71e394915b
amrit <amrit@192.168.1.39>
2024-07-09 15:40:18 +0530

    New values

---
+++ MavenSample1/src/main/java/sample/Methods.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class Methods 
+{
+	int a,b,sum,c=10;
+	public void getValue(int a,int b)
+	{
+		this.a=a;
+		this.b=b;	
+	}
+	public void arithmetics()
+	{
+		sum=a+b;
+		System.out.println("Sum:"+sum);
+	}
+}
---
+++ MavenSample1/src/main/java/sample/Operator.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Operator extends Methods{
+	public Operator()
+	{
+		System.out.println("Postincrement addition:"+ c++);
+		System.out.println("Preincrement addition:"+ ++c);
+		System.out.println("Postdecrement subtraction:"+ c--);
+		System.out.println("Predecrement subtraction:"+ --c);
+	}
+}
